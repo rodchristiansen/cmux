80441d0d632e15220d75286e305713d5ac30fa71
Lawrence Chen <lawrencecchen@users.noreply.github.com>
2026-04-01 16:31:34 -0700

    Start control socket for UI test launches

--- cmuxUITests/WorkspaceDescriptionUITests.swift
+++ cmuxUITests/WorkspaceDescriptionUITests.swift
@@ -102,6 +102,7 @@ final class WorkspaceDescriptionUITests: XCTestCase {
         app.launchEnvironment["CMUX_UI_TEST_GOTO_SPLIT_SETUP"] = "1"
         app.launchEnvironment["CMUX_UI_TEST_GOTO_SPLIT_PATH"] = dataPath
         app.launchEnvironment["CMUX_UI_TEST_FOCUS_SHORTCUTS"] = "1"
+        app.launchEnvironment["CMUX_UI_TEST_SOCKET_SANITY"] = "1"
         app.launchEnvironment["CMUX_SOCKET_PATH"] = socketPath
         app.launchEnvironment["CMUX_TAG"] = launchTag
         return app
